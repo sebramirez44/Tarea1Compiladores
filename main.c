9073d49103920b639bf87ba51d418f4f04aac520
sebramirez44 <sebramirezcordero@gmail.com>
2025-09-29 22:28:33 -0600

    started stack implementation

--- main.c
+++ main.c
@@ -2,8 +2,38 @@
 #include <stdlib.h>
 
 #include "queue.h"
+typedef struct StackNode {
+    void* data;
+    StackNode* next;
+} StackNode;
 
+typedef struct Stack {
+    StackNode* Top;
+} Stack;
 
+Stack* createStack() {
+    Stack* s = (Stack*)malloc(sizeof(Stack));
+    s->Top = NULL;
+
+    return s;
+};
+
+void stackPush(Stack* s, void* data) {
+    StackNode* newNode = (StackNode*)malloc(sizeof(StackNode));
+    newNode->data = data;
+
+    newNode->next = s->Top;
+    s->Top = newNode;
+};
+
+void* stackPop(Stack* s) {
+    StackNode* currTop = s->Top;
+    void* currTopValue = currTop->data;
+    s->Top = s->Top->next;
+    free(currTop);
+
+    return currTopValue;
+};
 
 int main() {
     Queue* q = createQueue();
