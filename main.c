c46a2b7f77a757925d8d05023b16183340f51a73
sebramirez44 <sebramirezcordero@gmail.com>
2025-09-29 22:39:32 -0600

    mostly finished stack implementation

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 #include "queue.h"
 typedef struct StackNode {
     void* data;
-    StackNode* next;
+    struct StackNode* next;
 } StackNode;
 
 typedef struct Stack {
@@ -27,12 +27,22 @@ void stackPush(Stack* s, void* data) {
 };
 
 void* stackPop(Stack* s) {
+    if (s == NULL || s->Top == NULL) {
+        return NULL;
+    }
     StackNode* currTop = s->Top;
     void* currTopValue = currTop->data;
     s->Top = s->Top->next;
     free(currTop);
 
     return currTopValue;
+};
+
+void destroyStack(Stack* s) {
+    while (s->Top != NULL) {
+        stackPop(s);
+    }
+    free(s);
 };
 
 int main() {
@@ -44,7 +54,17 @@ int main() {
     enqueue(q, num1);
     enqueue(q, num2);
     int* res= (int*)dequeue(q);
-    printf("%d", *res);
+    printf("%d\n\n", *res);
+
+    Stack* s = createStack();
+    int* num3 = malloc(sizeof(int));
+    *num3 = 30;
+    int* num4 = malloc(sizeof(int));
+    *num4 = 40;
+    stackPush(s, num3);
+    stackPush(s, num4);
+    int* res2 = (int*)stackPop(s);
+    printf("%d", *res2);
 
     return 0;
 };
